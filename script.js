245bfdd255246309bf6f6a8f626d92f6d6e03364
William Ruback <rubackw@gmail.com>
2021-12-21 14:34:50 -0500

    Added base development files.

---
+++ script.js
@@ -0,0 +1,15 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
