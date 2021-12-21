b0afa8bfa4f3ac9abfdb3d4b120f2311e8487c96
William Ruback <rubackw@gmail.com>
2021-12-21 15:19:21 -0500

    Added Length input, and base generator for the password.

--- script.js
+++ script.js
@@ -5,10 +5,34 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   
   passwordText.value = password;
 
+}
+
+
+function generatePassword(){
+  var passwordCharacters="";
+  var passwordLength=0;
+  var correct = false;
+
+  while (correct === false){
+    passwordLength = parseInt(prompt("How long do you want you password? (8 to 128)"));
+    if(passwordLength >= 8 && passwordLength <= 128){
+      correct = true;
+    }
+    else{
+      alert("Please enter a number between 8 and 128.");
+    }
+  }
+
+  passwordCharacters += ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+  var output = "";
+  for (let i=0; i<passwordLength; i++) {
+    let randLetter = Math.floor(Math.random() * passwordCharacters.length);
+    output += passwordCharacters[randLetter];
+  }
+  return output;
 }
 
 // Add event listener to generate button
