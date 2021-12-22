4dc578dd53c9db2a39e297f13137456394bc1ca4
William Ruback <rubackw@gmail.com>
2021-12-22 16:04:51 -0500

    Added code to guarantee that the output will contain all character type's selected.

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function generatePassword(){
   
   let passwordLength=0;
   let passwordCharacters="";
-  let output = "";
+  let gaps = ["●"];
 
   while (true){
     passwordLength = parseInt(prompt("How long do you want you password? (8 to 128)"));
@@ -30,20 +30,64 @@ function generatePassword(){
       alert("Please enter a number between 8 and 128.");
     }
   }
-
+  
+  for (let i=0; i<passwordLength-1; i++) {
+    gaps.push("●");
+  }
+  let placement = [];
   while (true){
     if (confirm("Would you like to add Uppercase characters?")){
       passwordCharacters += uppercase;
+      while(true){
+        let random = Math.floor(Math.random() * passwordLength);
+        if (placement === [] || !placement.includes(random)){
+          gaps[random] = uppercase[Math.floor(Math.random() * uppercase.length)];
+          placement.push(random);
+          break;
+        }
+      }
     }
+    console.log(placement);
+    console.log(gaps);
     if (confirm("Would you like to add Lowercase characters?")){
       passwordCharacters += lowercase;
+      while(true){
+        let random = Math.floor(Math.random() * passwordLength);
+        if (placement === [] || !placement.includes(random)){
+          gaps[random] = lowercase[Math.floor(Math.random() * lowercase.length)];
+          placement.push(random);
+          break;
+        }
+      }
     }
-    if (confirm("Would you like to add Numbers?")){
+    console.log(placement);
+    console.log(gaps);
+    if (confirm("Would you like to add numbers?")){
       passwordCharacters += numbers;
+      while(true){
+        let random = Math.floor(Math.random() * passwordLength);
+        if (placement === [] || !placement.includes(random)){
+          gaps[random] = numbers[Math.floor(Math.random() * numbers.length)];
+          placement.push(random);
+          break;
+        }
+      }
     }
+    console.log(placement);
+    console.log(gaps);
     if (confirm("Would you like to add Special characters?")){
       passwordCharacters += special;
+      while(true){
+        let random = Math.floor(Math.random() * passwordLength);
+        if (placement === [] || !placement.includes(random)){
+          gaps[random] = special[Math.floor(Math.random() * special.length)];
+          placement.push(random);
+          break;
+        }
+      }
     }
+    console.log(placement);
+    console.log(gaps);
     if(passwordCharacters !== ""){
       break;
     }
@@ -51,10 +95,16 @@ function generatePassword(){
       alert("Please select at least one kind of character to be in the password.")
     }
   }
-
+  console.log(gaps);
+  let output = "";
   for (let i=0; i<passwordLength; i++) {
-    let randLetter = Math.floor(Math.random() * passwordCharacters.length);
-    output += passwordCharacters[randLetter];
+    if(gaps[i] === "●"){
+      let randLetter = Math.floor(Math.random() * passwordCharacters.length);
+      output += passwordCharacters[randLetter];
+    }
+    else{
+      output += gaps[i];
+    }
   }
   return output;
 }
