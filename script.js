3746d15422cca499a4d89556be18940fb5f3bba9
William Ruback <rubackw@gmail.com>
2021-12-22 17:19:50 -0500

    Reformatted code to be easier to read, cleaned it up, and added comments.

--- script.js
+++ script.js
@@ -12,15 +12,39 @@ function writePassword() {
 
 
 function generatePassword(){
+  // These are the groups of characters that can be added to the password.
   let lowercase = "abcdefghijklmnopqrstuvwxyz";
   let uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let numbers = "1234567890";
   let special = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
   
-  let passwordLength=0;
-  let passwordCharacters="";
-  let gaps = ["●"];
+  // These are all the variables needed to run the program.
+  let passwordLength=0; // Will record how long they want the password.
+  let passwordCharacters=""; // Will contain all characters that could be added to the password.
+  let gaps = ["●","●","●","●","●","●","●","●"]; //Will contain and show all the pre-filled characters for the password.
+  let output = ""; // Will contain the final generated password.
 
+  // This function checks if the user want the character group in the password, 
+  // then guarantees thru the placement file that one of thoses character will 
+  // be in the password.
+  function checkAddCharacters(characterName,characters){
+    if (confirm("Would you like to add " + characterName + "?")){ // Asks if they want this character.
+      passwordCharacters += characters; // Add character to the pool if yes.
+      while(true){ //Loops until pre-fill character is added.
+        let random = Math.floor(Math.random() * passwordLength); // Picks a spot in the password.
+        if (gaps[random] === "●"){ // Checks if something has been placed there before.
+          gaps[random] = characters[Math.floor(Math.random() * characters.length)]; // If not, add random character from character group.
+          console.log(gaps);
+          break; // ends the loop after gap spot is pre-filled.
+        }
+        else{ // If that spot was filled, the segment re-runs, picking a new spot until it is open.
+          console.log(random + " Is already in placement! getting new number...");
+        }
+      }
+    }
+  }
+
+  // This segment asks for the password length, and loops until it gets a useable answer.
   while (true){
     passwordLength = parseInt(prompt("How long do you want you password? (8 to 128)"));
     if(passwordLength >= 8 && passwordLength <= 128){
@@ -31,63 +55,18 @@ function generatePassword(){
     }
   }
   
-  for (let i=0; i<passwordLength-1; i++) {
+  // This segment adds all the necessary circles to the gaps file to make it make the password length.
+  // There is a minus 8 because the gaps variable already has the first 8 included.
+  for (let i=0; i<passwordLength-8; i++) {
     gaps.push("●");
   }
-  let placement = [];
+  
+  // This checks what character groups the user wants. If they select none, it loops until they do.
   while (true){
-    if (confirm("Would you like to add Uppercase characters?")){
-      passwordCharacters += uppercase;
-      while(true){
-        let random = Math.floor(Math.random() * passwordLength);
-        if (placement === [] || !placement.includes(random)){
-          gaps[random] = uppercase[Math.floor(Math.random() * uppercase.length)];
-          placement.push(random);
-          break;
-        }
-      }
-    }
-    console.log(placement);
-    console.log(gaps);
-    if (confirm("Would you like to add Lowercase characters?")){
-      passwordCharacters += lowercase;
-      while(true){
-        let random = Math.floor(Math.random() * passwordLength);
-        if (placement === [] || !placement.includes(random)){
-          gaps[random] = lowercase[Math.floor(Math.random() * lowercase.length)];
-          placement.push(random);
-          break;
-        }
-      }
-    }
-    console.log(placement);
-    console.log(gaps);
-    if (confirm("Would you like to add numbers?")){
-      passwordCharacters += numbers;
-      while(true){
-        let random = Math.floor(Math.random() * passwordLength);
-        if (placement === [] || !placement.includes(random)){
-          gaps[random] = numbers[Math.floor(Math.random() * numbers.length)];
-          placement.push(random);
-          break;
-        }
-      }
-    }
-    console.log(placement);
-    console.log(gaps);
-    if (confirm("Would you like to add Special characters?")){
-      passwordCharacters += special;
-      while(true){
-        let random = Math.floor(Math.random() * passwordLength);
-        if (placement === [] || !placement.includes(random)){
-          gaps[random] = special[Math.floor(Math.random() * special.length)];
-          placement.push(random);
-          break;
-        }
-      }
-    }
-    console.log(placement);
-    console.log(gaps);
+    checkAddCharacters("Uppercase characters",uppercase);
+    checkAddCharacters("Lowercase characters",lowercase);
+    checkAddCharacters("Numbers",numbers);
+    checkAddCharacters("Special characters",special);
     if(passwordCharacters !== ""){
       break;
     }
@@ -95,18 +74,17 @@ function generatePassword(){
       alert("Please select at least one kind of character to be in the password.")
     }
   }
-  console.log(gaps);
-  let output = "";
+
+  // This builds the password.
   for (let i=0; i<passwordLength; i++) {
-    if(gaps[i] === "●"){
-      let randLetter = Math.floor(Math.random() * passwordCharacters.length);
-      output += passwordCharacters[randLetter];
+    if(gaps[i] === "●"){ // Checks if this letter was pre-filled.
+      output += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)]; // if not, generate a random character from the combined selected groups. 
     }
     else{
-      output += gaps[i];
+      output += gaps[i]; // if so, add the pre-fill character to that spot.
     }
   }
-  return output;
+  return output; // return the generated password.
 }
 
 // Add event listener to generate button
